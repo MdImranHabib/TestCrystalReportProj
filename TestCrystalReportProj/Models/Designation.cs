395be07143b16b67d9933c8df8d0335ef0a3c93e
MdImranHabib <16103023@iubat.edu>
2021-04-12 17:47:23 +0600

    trying to connect oracledb

---
+++ TestCrystalReportProj/Models/Designation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestCrystalReportProj.Models
+{
+    public class Designation
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+    }
+}
---
+++ TestCrystalReportProj/Models/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestCrystalReportProj.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(150)]
+        public string Address { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Designation")]
+        public int DesignationId { get; set; }
+
+        public Designation Designation { get; set; }
+    }
+}
