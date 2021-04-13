a41d16a43e5600c7fb3ae372817ea54bc62b798b
MdImranHabib <16103023@iubat.edu>
2021-04-13 10:31:46 +0600

    working on oracle databse connection

--- TestCrystalReportProj/Data/ApplicationDbContext.cs
+++ TestCrystalReportProj/Data/ApplicationDbContext.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TestCrystalReportProj.Models;
 
 namespace TestCrystalReportProj.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDBContext : DbContext
     {
+        public ApplicationDBContext (DbContextOptions<ApplicationDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TestCrystalReportProj.Models.Designation> Designation { get; set; }
     }
 }
